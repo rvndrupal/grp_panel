9c554563306cd67a73a82ee27d52b1ee29a02247
rvndrupal <rodrigodrupal1@gmail.com>
2019-04-03 17:44:47 -0600

    Inicio del respaldo

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models\Role; //agragar rol
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $this->call(PermissionsTableSeeder::class);
+         factory(App\Product::class, 100)->create();
+         factory(App\User::class, 1)->create();
+
+
+         Role::create([
+            'name'  => 'Admin',
+            'slug'  => 'admin',
+            'special' => 'all-access'
+        ]);
+
+        App\User::create([
+            'name'=>'rodrigo',
+            'email'=>'rodrigo@gmail.com',
+            'password'=>bcrypt('rorro13'),
+
+        ]);
+    }
+}
